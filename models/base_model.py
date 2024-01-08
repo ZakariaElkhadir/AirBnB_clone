0f590e9367d91c51e03b63c42632c59b1256df98
Parrot Live user <user@localhost.localdomain>
2024-01-08 23:02:58 +0000

    add file

---
+++ models/base_model.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Class Model"""
+
+from uuid import uuid4
+from datetime import datetime
+
+
+class BaseModel:
+    """
+    class Base model that defines all
+    common attributes/methods for other
+    classes
+    """
+
+    def __init__(self):
+        """initialization of BaseModel class"""
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Returns the string representation of an instance"""
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """set updated_at to now datetime"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """returns the dictionary containing all keys/values"""
+        ToDict = dict(self.__dict__)
+        ToDict["__class__"] = self.__class__.__name__
+        if not isinstance(ToDict["created_at"], str):
+            ToDict["created_at"] = ToDict["created_at"].isoformat()
+        if not isinstance(ToDict["updated_at"], str):
+            ToDict["updated_at"] = ToDict["updated_at"].isoformat()
+        return ToDict
